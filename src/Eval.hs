fa65a706048d77fbea011c28fa3f603a35ea0afe
Francesco Zanitti <fzanitti@gmail.com>
2016-06-30 09:48:43 +0200

    extracted env functions from Eval

--- src/Eval.hs
+++ src/Eval.hs
@@ -4,7 +4,6 @@
 module Eval
     ( eval
     , primitiveBindings
-    , bindVars
     ) where
 
 import           Control.Lens         hiding (List, cons, noneOf)
@@ -12,6 +11,7 @@ import           Control.Lens.Extras  (is)
 import           Control.Monad.Except
 import           Data.IORef
 import           Data.Maybe           (fromMaybe, isJust, isNothing)
+import           Env
 import           Lib                  (liftThrows)
 import           Parser               (readExpr, readExprList)
 import           System.IO
@@ -68,42 +68,6 @@ makeFunc :: Maybe String -> Env -> [LispVal] -> [LispVal] -> IOThrowsError LispV
 makeFunc varargs env params body = return $ Func (map show params) varargs body env
 makeNormalFunc = makeFunc Nothing
 makeVarArgs = makeFunc . Just . show
-
-isBound :: Env -> String -> IO Bool
-isBound envRef var = fmap (isJust . lookup var) (readIORef envRef)
-
-getVar :: Env -> String -> IOThrowsError LispVal
-getVar envRef var = do env <- liftIO $ readIORef envRef
-                       maybe (throwError $ UnboundVar "Getting an unbound variable" var)
-                             (liftIO . readIORef)
-                             (lookup var env)
-
-setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
-setVar envRef var value = do env <- liftIO $ readIORef envRef
-                             maybe (throwError $ UnboundVar "Setting an unbound variable" var)
-                                   (liftIO . flip writeIORef value)
-                                   (lookup var env)
-                             return value
-
-defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
-defineVar envRef var value = do
-  alreadyDefined <- liftIO $ isBound envRef var
-  if alreadyDefined
-    then setVar envRef var value >> return value
-    else liftIO $ do
-      valueRef <- newIORef value
-      env <- readIORef envRef
-      writeIORef envRef ((var, valueRef) : env)
-      return value
-
-bindVars :: Env -> [(String, LispVal)] -> IO Env
-bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
-  where extendEnv bindings env = fmap (++ env) (mapM addBinding bindings)
-        addBinding (var, value) = do ref <- newIORef value
-                                     return (var, ref)
-
-nullEnv :: IO Env
-nullEnv = newIORef []
 
 apply :: LispVal -> [LispVal] -> IOThrowsError LispVal
 apply (PrimitiveFunc func) args = liftThrows $ func args
